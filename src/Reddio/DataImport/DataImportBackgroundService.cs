62ecf08ec8a2ecf65ab3063c225df0e756fa1937
Yavor Fingarov <yavor.fingarov@outlook.com>
2023-02-11 12:20:51 +0200

    Refactored background service

---
+++ src/Reddio/DataImport/DataImportBackgroundService.cs
@@ -0,0 +1,43 @@
+﻿namespace Reddio.DataImport
+{
+    public sealed class DataImportBackgroundService : BackgroundService
+    {
+        private readonly ILogger<DataImportBackgroundService> _Logger;
+
+        private readonly DataImportConfiguration _DataImportConfiguration;
+
+        private readonly IServiceProvider _ServiceProvider;
+
+        public DataImportBackgroundService(ILogger<DataImportBackgroundService> logger,
+            DataImportConfiguration dataImportConfiguration,
+            IServiceProvider serviceProvider)
+        {
+            _Logger = logger;
+            _DataImportConfiguration = dataImportConfiguration;
+            _ServiceProvider = serviceProvider;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using var timer = new PeriodicTimer(TimeSpan.FromHours(_DataImportConfiguration.HostedServicePeriod));
+            do
+            {
+                using var scope = _ServiceProvider.CreateScope();
+                var dataImportHandler = scope.ServiceProvider.GetRequiredService<IDataImportHandler>();
+                await ImportDataAsync(dataImportHandler, stoppingToken);
+            } while (await timer.WaitForNextTickAsync(stoppingToken) && !stoppingToken.IsCancellationRequested);
+        }
+
+        public async Task ImportDataAsync(IDataImportHandler dataImportHandler, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await dataImportHandler.HandleAsync(cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                _Logger.LogError(ex, "Could not import data.");
+            }
+        }
+    }
+}
---
+++ tests/Reddio.UnitTests/DataImport/DataImportBackgroundServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using Reddio.DataImport;
+
+namespace Reddio.UnitTests.DataImport
+{
+    public class DataImportBackgroundServiceTests : TestBaseFor<DataImportBackgroundService>
+    {
+        private readonly DataImportBackgroundService _DataImportBackgroundService;
+
+        private readonly Mock<IDataImportHandler> _DataImportHandlerMock;
+
+        public DataImportBackgroundServiceTests()
+        {
+            _DataImportHandlerMock = new Mock<IDataImportHandler>(MockBehavior.Strict);
+            _DataImportBackgroundService = new DataImportBackgroundService(LoggerMock.Object,
+                DataImportConfiguration, Mock.Of<IServiceProvider>(MockBehavior.Strict));
+        }
+
+        [Fact]
+        public async Task ImportDataAsync_LogsException_WhenDataImportHandlerThrows()
+        {
+            LoggerMock.Setup(LogLevel.Error);
+            var exception = new Exception("test");
+            _DataImportHandlerMock.Setup(h => h.HandleAsync(CancellationToken.None)).Throws(exception);
+
+            await _DataImportBackgroundService.ImportDataAsync(_DataImportHandlerMock.Object, CancellationToken.None);
+
+            _DataImportHandlerMock.Verify(h => h.HandleAsync(It.IsAny<CancellationToken>()), Times.Once);
+            LoggerMock.Verify(LogLevel.Error, "Could not import data.", exception);
+            LoggerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ImportDataAsync_CallsDataImportHandler()
+        {
+            _DataImportHandlerMock.Setup(h => h.HandleAsync(CancellationToken.None)).Returns(Task.CompletedTask);
+
+            await _DataImportBackgroundService.ImportDataAsync(_DataImportHandlerMock.Object, CancellationToken.None);
+
+            _DataImportHandlerMock.Verify(h => h.HandleAsync(It.IsAny<CancellationToken>()), Times.Once);
+        }
+    }
+}
